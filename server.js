db0d6d782d5564051b91ae6d0bd8fc90cd271862
Christine Baxter <christine.d.baxter@live.com>
2022-01-29 21:27:39 -0600

    Deploying

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
